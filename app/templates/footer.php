8b5a07217994a2a5a6adfc08ed5510fc0a7a9b9d
daanigp <danielgarciapascual23@gmail.com>
2026-05-03 21:34:08 +0200

    menú footer - redes sociales

--- app/templates/footer.php
+++ app/templates/footer.php
@@ -1,5 +1,17 @@
     <footer>
         <h1>Redes sociales</h1>
+        <ul class="menu-rrss">
+            <li>
+                <a href="https://github.com/daanigp" target="_blank">
+                    <i class="fa-brands fa-github"></i>
+                </a>
+            </li>
+            <li>
+                <a href="https://www.linkedin.com/in/daanigp" target="_blank">
+                    <i class="fa-brands fa-square-linkedin"></i>
+                </a>
+            </li>
+        </ul>
     </footer>
 </body>
 </html>
