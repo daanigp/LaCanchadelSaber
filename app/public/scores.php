aa2a1f1922a5dfe52acc7a138d7c82e3eb9a3267
daanigp <danielgarciapascual23@gmail.com>
2026-05-13 19:46:19 +0200

    feat: nuevo logo + reestruccturación

---
+++ app/public/scores.php
@@ -0,0 +1,14 @@
+<?php
+    $css = "../style/styleScores.css";
+    require_once(__DIR__. "/../templates/header.php");
+?>
+
+    <main>
+        <div class="layout">
+            <h1>PUNTUACIONES</h1>
+        </div>
+    </main>
+
+<?php
+    require_once(__DIR__. "/../templates/footer.php");
+?>
--- app/templates/header.php
+++ app/templates/header.php
@@ -6,20 +6,26 @@
     <title>LaCanchaDelSaber</title>
     <link rel="stylesheet" href="../style/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="shortcut icon" href="../assets/icon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="../assets/logo-LCDS.ico" type="image/x-icon">
     <?php if(isset($css)) { ?>
         <link rel="stylesheet" href="<?= $css ?>">
     <?php } ?>
 </head>
 <body>
     <header>
-        <h1>La Cancha del Saber</h1>
+        <img 
+            src="../assets/logo-header-LCDS.png" 
+            alt="Logo de La Cancha del Saber"
+            width="300px"
+            height="125px"
+        >
         <div class="dropdown-menu">
             <input type="checkbox" id="hamburguesa">
             <label for="hamburguesa" class="fa fa-bars" id="icono"></label>
             <ul class="menu">
                 <li><a href="../public/index.php">Index</a></li>
-                <li><a href="../public/game.php">Game</a></li>
+                <li><a href="../public/game.php">Juego</a></li>
+                <li><a href="../public/scores.php">Puntuaciones</a></li>
                 <li><a href="../public/about_us.php">Sobre nosotros</a></li>
                 <li><a href="../public/login.php">Login</a></li>
             </ul>
